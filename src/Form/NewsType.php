e4ad328d2b7344eb1a8dad138cd19f71ca28bc7c
Jonas Poli <jonaspoli@gmail.com>
2024-02-20 10:28:51 -0300

    Ajuste nas categorias

--- src/Form/NewsType.php
+++ src/Form/NewsType.php
@@ -49,8 +49,9 @@ class NewsType extends AbstractType
             ])
             ->add('category', EntityType::class, [
                 'class' => NewsCategory::class,
-                'choice_label' => 'id',
+                'choice_label' => 'title',
                 'multiple' => true,
+                'expanded'=> true,
             ])
         ;
     }
